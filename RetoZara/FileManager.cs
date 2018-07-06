c19043e8334bc57c9dce0346bc65e08116e524be
Jose Alamo <alamose@hotmail.com>
2018-07-06 04:52:06 +0200

    metodo DateConverter implementado

--- RetoZara/FileManager.cs
+++ RetoZara/FileManager.cs
@@ -6,46 +6,63 @@ using System.Data;
 
 namespace RetoZara
 {
-	class FileManager
+	public class FileManager
 	{
-
-		protected void ImportCSV(string csvPath)
+		string[] calendar = {"ene", "feb", "mar" ,"abr", "may", "jun",
+			"jul", "ago", "sep", "oct", "nov", "dic" };
+		public DateTime DateConverter(string date)
 		{
-
-			//Create a DataTable.  
-			DataTable dt = new DataTable();
-			dt.Columns.AddRange(new DataColumn[5] { new DataColumn("Id", typeof(int)),
-		new DataColumn("Name", typeof(string)),
-		new DataColumn("Technology", typeof(string)),
-		new DataColumn("Company", typeof(string)),
-		new DataColumn("Country",typeof(string)) });
-
-			//Read the contents of CSV file.  
-			string csvData = File.ReadAllText(csvPath);
-
-			//Execute a loop over the rows.  
-			foreach (string row in csvData.Split('\n'))
-			{
-				if (!string.IsNullOrEmpty(row))
-				{
-					dt.Rows.Add();
-					int i = 0;
-
-					//Execute a loop over the columns.  
-					foreach (string cell in row.Split(','))
-					{
-						dt.Rows[dt.Rows.Count - 1][i] = cell;
-						i++;
-					}
-				}
-			}
-
-			//Bind the DataTable.  
-			//GridView1.DataSource = dt;
-			//GridView1.DataBind();
+			var values = date.Split('-');
+			int month = Array.IndexOf(calendar,values[1]);
+			return new DateTime(Int32.Parse(values[2]), month + 1, Int32.Parse(values[0]));
 		}
+
+		//public void ImportCSV(string csvPath)
+		//{
+		//	string[] tempStringA;
+		//	var tempDate;
+		//	var tempShare;
+		//	//Create a DataTable.  
+		//	DataTable dt = new DataTable();
+		//	dt.Columns.AddRange(new DataColumn[3] 
+		//	{new DataColumn("Fecha", typeof(string)),
+		//	new DataColumn("Apertura", typeof(string)),
+		//	new DataColumn("Cierre",typeof(string)) });
+
+		//	//Read the contents of CSV file.  
+		//	string csvData = File.ReadAllText(csvPath);
+
+		//	//Execute a loop over the rows.  
+		//	foreach (string row in csvData.Split('\n'))
+		//	{
+		//		if (!string.IsNullOrEmpty(row))
+		//		{
+		//			dt.Rows.Add();
+		//			tempStringA = row.Split(',');
+		//			tempDate = new DateTime()
+		//			dt.Rows[dt.Rows.Count - 1][0] = ;
+
+
+		//			int i = 0;
+
+		//			//Execute a loop over the columns.  
+		//			foreach (string cell in row.Split(','))
+		//			{
+		//				dt.Rows[dt.Rows.Count - 1][i] = cell;
+		//				i++;
+		//			}
+		//		}
+		//	}
+
+		//	//Bind the DataTable.  
+		//	//GridView1.DataSource = dt;
+		//	//GridView1.DataBind();
+		//}
 		static void Main(string[] args)
 		{
+			FileManager fm = new FileManager();
+			Console.WriteLine(fm.DateConverter("28-dic-2017").ToString());
+			Console.ReadLine();
 		}
 	}
 }
